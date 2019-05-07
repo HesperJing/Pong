4612a0271a46e44f16224fff833fbf7174d0922d
HesperJing <50358571+HesperJing@users.noreply.github.com>
2019-05-07 09:51:12 +0100

    Update led.v

--- led.v
+++ led.v
@@ -3,10 +3,10 @@ module led (
 	 input reset,
 	 input right,
 	 input left,
-	 output wire [9:1] led
+	output wire [9:1] led    //9 LEDs (1 to 9) used to blink when a player wins.
 	 );
 	 
-	 reg [24:0] counter = 25'b0;
+	reg [24:0] counter = 25'b0;    //System clock is 50MHz, which is too high for eyes to see leds blinking. This counter is used to divide frequency. Blinker's frequency is 50MHz/2^25=1.49Hz. 
 	 reg enable;
 	 reg [9:1] led_loop;
 	 wire enable_loop;
@@ -16,7 +16,7 @@ module led (
 	 
 	 always @ (posedge clock) begin
 	     if (reset) begin
-		      counter <= 25'b0;
+		      counter <= 25'b0;    //If reset, counter starts over from 0.   
 		  end else begin
 		      counter <= counter+1'b1;
 		  end
@@ -25,7 +25,7 @@ module led (
 	 always @ (posedge clock) begin
 	     if (reset) begin
 		      enable <= 1'b0;
-		  end else if (counter==25'hffffff) begin
+	     end else if (counter==25'hffffff) begin    //When counter is full, provides a enable signal to light up leds.
 		      enable <= 1'b1;
 		  end else begin
 		      enable <= 1'b0;
@@ -34,11 +34,11 @@ module led (
 	 
 	 always @ (posedge clock) begin
 	     if (reset) begin
-		      led_loop <= 9'b100000000;
+		      led_loop <= 9'b100000000;    //Light up the led9 at first.
 		  end else if(enable_loop && left) begin
-				led_loop <= {led_loop[1], led_loop[9:2]};
+			  led_loop <= {led_loop[1], led_loop[9:2]};    //Leds light up from left to right in turn.
 		  end else if(enable_loop && right) begin
-				led_loop <= {led_loop[8:1], led_loop[9]};
+			  led_loop <= {led_loop[8:1], led_loop[9]};    //Leds light up from right to left in turn.
 		  end
 	 end
 
